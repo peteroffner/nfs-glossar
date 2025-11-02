76c3fb9f6cd1cfe17085f436f5a6620f0d6d772b
peteroffner <151682974+peteroffner@users.noreply.github.com>
2025-11-02 21:30:59 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("notsan-glossar").then(cache => {
+      return cache.addAll([".", "index.html", "manifest.json", "krankheitsbilder.json"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
